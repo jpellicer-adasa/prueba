be3be2b7b82abbd787b5d77f38dc24c4cd728569
joseangel.pellicer <joseangel.pellicer@JPELLICER-P01.Adasa.local>
2016-03-15 19:25:35 +0100

    Packed libs

---
+++ src/main/java/com/adasasistemas/pilot/webtest3/TestClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adasasistemas.pilot.webtest3;
+
+/**
+ *
+ * @author joseangel.pellicer
+ */
+public class TestClass {
+    
+    public String getOne(){
+        return "one";
+    }
+    
+    public String getTwo(){
+        return "noTwo";
+    }
+    
+}
---
+++ src/test/java/com/adasasistemas/pilot/webtest3/TestClassTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adasasistemas.pilot.webtest3;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author joseangel.pellicer
+ */
+public class TestClassTest {
+    
+    public TestClassTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getOne method, of class TestClass.
+     */
+    @Test
+    public void testGetOne() {
+        System.out.println("getOne");
+        TestClass instance = new TestClass();
+        String expResult = "one";
+        String result = instance.getOne();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+     //   fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTwo method, of class TestClass.
+     */
+    @Test
+    public void testGetTwo() {
+        System.out.println("getTwo");
+        TestClass instance = new TestClass();
+        String expResult = "two";
+        String result = instance.getTwo();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
