c6d77713e5d425e5d96b951f6d6156b743f04b99
joseangel.pellicer <joseangel.pellicer@JPELLICER-P01.Adasa.local>
2016-03-16 10:52:39 +0100

    Packed libs

--- src/test/java/com/adasasistemas/pilot/webtest3/TestClassTest.java
+++ src/test/java/com/adasasistemas/pilot/webtest3/TestClassTest.java
@@ -11,6 +11,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  *
@@ -63,6 +67,30 @@ public class TestClassTest {
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
+    }
+      @Test
+    public void testSimple() throws Exception {
+        // Create a new instance of the Firefox driver
+        // Notice that the remainder of the code relies on the interface, 
+        // not the implementation.
+        WebDriver driver = new FirefoxDriver();
+
+        // And now use this to visit NetBeans
+        driver.get("http://www.netbeans.org");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("http://www.netbeans.org");
+
+        // Check the title of the page
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().contains("NetBeans");
+            }
+        });
+
+        //Close the browser
+        driver.quit();
     }
     
 }
